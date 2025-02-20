f38a1113daa3c017a021ad12f24d9d4846dd6c1d
LuisAlbertoIT <20212380@itla.edu.do>
2025-02-20 01:49:59 -0400

    Dinamic Grid Generation
    temporary tilemap
    grid generation
    tile overlay and cursor

---
+++ Assets/Movement/Scripts/MapManager.cs
@@ -0,0 +1,57 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class MapManager : MonoBehaviour
+{
+    private static MapManager _instance;
+    public static MapManager Instance {  get { return _instance; } }
+    
+    public OverlayTile overlayTilePrefab;
+    public GameObject overlayContainer;
+
+    public Dictionary<Vector2Int, OverlayTile> map;
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this ){
+            Destroy( this.gameObject );
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        var tilemap = gameObject.GetComponentInChildren<Tilemap>();
+        map = new Dictionary<Vector2Int, OverlayTile>();
+
+        BoundsInt bounds = tilemap.cellBounds;
+
+        //looping through all of our tiles
+ //       for (int z = bounds.max.z; z >= bounds.min.z; z--)
+ //       {
+            for (int y = bounds.min.y; y < bounds.max.y; y++)
+            {
+                for (int x = bounds.min.x; x < bounds.max.x; x++)
+                {
+                    var tileLocation = new Vector3Int(x, y, 0);
+                    //var tilekey = new Vector2Int(x, y);
+
+                    if (tilemap.HasTile(tileLocation)) //&& !map.ContainsKey(tilekey))
+                    {
+                        var overlayTile = Instantiate(overlayTilePrefab, overlayContainer.transform);
+                        var cellWorldPosition = tilemap.GetCellCenterWorld(tileLocation);
+
+                        overlayTile.transform.position = new Vector3(cellWorldPosition.x, cellWorldPosition.y, cellWorldPosition.z);
+                        overlayTile.GetComponent<SpriteRenderer>().sortingOrder = tilemap.GetComponent<TilemapRenderer>().sortingOrder;
+                        //map.Add(tilekey, overlayTile);
+                    }
+                }
+            }
+ //       }
+    }
+}
---
+++ Assets/Movement/Scripts/OverlayTile.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class OverlayTile : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            HideTile();
+        }
+    }
+
+    public void ShowTile()
+    {
+        gameObject.GetComponent<SpriteRenderer>().color = new  Color(1, 1, 1, 1);
+    }
+
+    public void HideTile()
+    {
+        gameObject.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 0);
+    }
+}
