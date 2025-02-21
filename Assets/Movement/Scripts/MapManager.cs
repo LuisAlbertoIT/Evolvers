28d9894184066d5b046d574263799236687d261b
LuisAlbertoIT <20212380@itla.edu.do>
2025-02-20 20:07:53 -0400

    Movimiento
    falta colocar y no reconocer los obstáculos

--- Assets/Movement/Scripts/MapManager.cs
+++ Assets/Movement/Scripts/MapManager.cs
@@ -39,7 +39,7 @@ public class MapManager : MonoBehaviour
                 for (int x = bounds.min.x; x < bounds.max.x; x++)
                 {
                     var tileLocation = new Vector3Int(x, y, 0);
-                    //var tilekey = new Vector2Int(x, y);
+                    var tilekey = new Vector2Int(x, y);
 
                     if (tilemap.HasTile(tileLocation)) //&& !map.ContainsKey(tilekey))
                     {
@@ -48,10 +48,52 @@ public class MapManager : MonoBehaviour
 
                         overlayTile.transform.position = new Vector3(cellWorldPosition.x, cellWorldPosition.y, cellWorldPosition.z);
                         overlayTile.GetComponent<SpriteRenderer>().sortingOrder = tilemap.GetComponent<TilemapRenderer>().sortingOrder;
-                        //map.Add(tilekey, overlayTile);
+                        overlayTile.gridLocation = tileLocation;
+                        map.Add(tilekey, overlayTile);
                     }
                 }
             }
  //       }
+    }
+
+    public List<OverlayTile> GetNeighbourTiles(OverlayTile currentOverlayTile)
+    {
+        var map = MapManager.Instance.map;
+
+        List<OverlayTile> neighbours = new List<OverlayTile>();
+
+        //top neighbour
+        Vector2Int locationToCheck = new Vector2Int(currentOverlayTile.gridLocation.x, currentOverlayTile.gridLocation.y + 1);
+
+        if (map.ContainsKey(locationToCheck))
+        {
+            neighbours.Add(map[locationToCheck]);
+        }
+
+        //bottom neighbour
+        locationToCheck = new Vector2Int(currentOverlayTile.gridLocation.x, currentOverlayTile.gridLocation.y - 1);
+
+        if (map.ContainsKey(locationToCheck))
+        {
+            neighbours.Add(map[locationToCheck]);
+        }
+
+        //right neighbour
+        locationToCheck = new Vector2Int(currentOverlayTile.gridLocation.x + 1, currentOverlayTile.gridLocation.y);
+
+        if (map.ContainsKey(locationToCheck))
+        {
+            neighbours.Add(map[locationToCheck]);
+        }
+
+        //left neighbour
+        locationToCheck = new Vector2Int(currentOverlayTile.gridLocation.x - 1, currentOverlayTile.gridLocation.y);
+
+        if (map.ContainsKey(locationToCheck))
+        {
+            neighbours.Add(map[locationToCheck]);
+        }
+
+        return neighbours;
     }
 }
--- Assets/Movement/Scripts/MouseController.cs
+++ Assets/Movement/Scripts/MouseController.cs
@@ -1,12 +1,22 @@
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
 public class MouseController : MonoBehaviour
 {
+    public GameObject characterPrefab;
+    private CharacterInfo character;
+    public float speed;
+
+    private Pathfinder pathfinder;
+    private List<OverlayTile> path = new List<OverlayTile>();
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        pathfinder = new Pathfinder();
     }
 
     // Update is called once per frame
@@ -16,15 +26,53 @@ public class MouseController : MonoBehaviour
 
         if(focusedTileHit.HasValue)
         {
-            GameObject overlayTile = focusedTileHit.Value.collider.gameObject;
+            OverlayTile overlayTile = focusedTileHit.Value.collider.gameObject.GetComponent<OverlayTile>();
             transform.position = new Vector3(overlayTile.transform.position.x, overlayTile.transform.position.y, overlayTile.transform.position.z-1);
             gameObject.GetComponent<SpriteRenderer>().sortingOrder = overlayTile.GetComponent<SpriteRenderer>().sortingOrder;
 
             if (Input.GetMouseButtonDown(0))
             {
-                overlayTile.GetComponent<OverlayTile>().ShowTile();
+                //overlayTile.GetComponent<OverlayTile>().ShowTile();
+                overlayTile.ShowTile();
+
+                if(character == null)
+                {
+                    character = Instantiate(characterPrefab).GetComponent<CharacterInfo>();
+                    PositionCharacterOnTile(overlayTile);
+                }
+                else
+                {
+                    path = pathfinder.FindPath(character.activeTile, overlayTile);
+                }
             }
         }
+
+        if(path.Count > 0)
+        {
+            MoveAlongPath();
+        }
+    }
+
+    private void MoveAlongPath()
+    {
+        var step = speed * Time.deltaTime;
+
+        var zIndex = path[0].transform.position.z;
+        character.transform.position = Vector2.MoveTowards(character.transform.position, path[0].transform.position, step);
+        character.transform.position = new Vector3(character.transform.position.x, character.transform.position.y, zIndex);
+
+        if(Vector2.Distance(character.transform.position, path[0].transform.position) < 0.0001f)
+        {
+            PositionCharacterOnTile(path[0]);
+            path.RemoveAt(0);
+        }
+    }
+
+    private void PositionCharacterOnTile(OverlayTile tile)
+    {
+        character.transform.position = new Vector3(tile.transform.position.x, tile.transform.position.y, tile.transform.position.z);
+        character.GetComponent<SpriteRenderer>().sortingOrder = tile.GetComponent<SpriteRenderer>().sortingOrder;
+        character.activeTile = tile;
     }
 
     public RaycastHit2D? GetFocusedOnTile()
