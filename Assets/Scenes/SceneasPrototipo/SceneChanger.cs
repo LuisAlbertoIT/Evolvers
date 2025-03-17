6b0c92a5f95f9c28e176013ab0c91f004d5caa02
Say2019 <Saylo21yt@gmail.com>
2025-03-17 14:10:14 +0100

    MenuPrototipo
    d

--- Assets/Scenes/SceneasPrototipo/SceneChanger.cs
+++ Assets/Scenes/SceneasPrototipo/SceneChanger.cs
@@ -3,10 +3,10 @@ using UnityEngine;
 public class SceneChanger : MonoBehaviour
 {
     //Funcion que carga la escena por nombre
-    public void ChangeScene(string sceneName)
+    public void ChangeScene(string Base)
     {
         //Carga la escena con el nombre que se le pase
-        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+        UnityEngine.SceneManagement.SceneManager.LoadScene(Base);
     }
 
 }
