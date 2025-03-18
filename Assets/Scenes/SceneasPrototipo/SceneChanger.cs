2260ca7ad32bed654e27575e30b504540e6b4a6b
ImNegary <halonexz@hotmail.com>
2025-03-18 10:09:04 -0400

    Prototipo mainMenu

--- Assets/Scenes/SceneasPrototipo/SceneChanger.cs
+++ Assets/Scenes/SceneasPrototipo/SceneChanger.cs
@@ -3,10 +3,10 @@ using UnityEngine;
 public class SceneChanger : MonoBehaviour
 {
     //Funcion que carga la escena por nombre
-    public void ChangeScene(string Base)
+    public void ChangeScene(string sceneName)
     {
         //Carga la escena con el nombre que se le pase
-        UnityEngine.SceneManagement.SceneManager.LoadScene(Base);
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
     }
 
 }
