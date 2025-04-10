0f4d40b2a38cc615af9993331df743b9a0368a08
Say2019 <Saylo21yt@gmail.com>
2025-04-10 05:11:27 +0200

    TRANSICIONES de ESCENA

--- Assets/Scenes/SceneasPrototipo/SceneChanger.cs
+++ Assets/Scenes/SceneasPrototipo/SceneChanger.cs
@@ -1,19 +1,40 @@
+using System.Collections;
+using System.Transactions;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour
 {
     [SerializeField] private string nombreEscena;
+    [SerializeField] private float transitionTime = 1f;
+
+    private Animator transitionAnimator;
+
+    private void Start()
+    {
+        transitionAnimator = GetComponentInChildren<Animator>();
+    }
 
     public void CargarEscena(string nombreEscena)
     {
-        SceneManager.LoadScene(nombreEscena);
+        StartCoroutine(SceneLoad(nombreEscena));
+
     }
 
     public void EnviarCriaturasAEscena(string nombreEscena)
     {
+       
         DontDestroyOnLoad(GameManager.instancia.gameObject);
         SceneManager.LoadScene(nombreEscena);
     }
+
+    public IEnumerator SceneLoad (string nombreEscena)
+    {
+        transitionAnimator.SetTrigger("StartTranstion");
+        yield return new WaitForSeconds(transitionTime);
+        SceneManager.LoadScene(nombreEscena);
+
+    }
+
 }
 
